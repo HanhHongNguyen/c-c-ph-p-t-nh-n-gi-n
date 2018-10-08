bfc665b8a49691200a26abaa79ecf10cb0f37521
hanh nguyen <1651010054hanh@ou.edu.vn>
2018-10-08 22:07:32 +0700

    Dang nhap

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace các_phép_tính_đơn_giản
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.btdangnhap = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btdangnhap);
+            this.groupBox1.Controls.Add(this.txtpass);
+            this.groupBox1.Controls.Add(this.txtname);
+            this.groupBox1.Location = new System.Drawing.Point(4, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(278, 178);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtname
+            // 
+            this.txtname.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtname.Location = new System.Drawing.Point(10, 25);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(262, 31);
+            this.txtname.TabIndex = 0;
+            // 
+            // txtpass
+            // 
+            this.txtpass.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpass.Location = new System.Drawing.Point(10, 77);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(262, 31);
+            this.txtpass.TabIndex = 0;
+            // 
+            // btdangnhap
+            // 
+            this.btdangnhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btdangnhap.Location = new System.Drawing.Point(81, 130);
+            this.btdangnhap.Name = "btdangnhap";
+            this.btdangnhap.Size = new System.Drawing.Size(128, 34);
+            this.btdangnhap.TabIndex = 1;
+            this.btdangnhap.Text = "Đăng nhập";
+            this.btdangnhap.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 184);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btdangnhap;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.TextBox txtname;
+    }
+}
+
